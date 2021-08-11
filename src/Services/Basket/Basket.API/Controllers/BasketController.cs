275f1fac029d603973427a7c1b8278ffe5a4ec2f
tolu omoseyin <toluomoseyin01@gmail.com>
2021-08-11 09:24:36 +0100

    finished discount api microservice

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,10 +1,7 @@
 ﻿using Basket.API.Entities;
 using Basket.API.Repositories;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
