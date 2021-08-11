893cdd62b97f6d88d230971978f042956f5b73fe
tolu omoseyin <toluomoseyin01@gmail.com>
2021-08-11 22:18:40 +0100

    made a change

--- src/Services/Discount/Discount.API/Startup.cs
+++ src/Services/Discount/Discount.API/Startup.cs
@@ -30,7 +30,7 @@ namespace Discount.API
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Discount.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Discount.API", Version = "v1" });                                                 
             });
         }
                                                                                                                             
