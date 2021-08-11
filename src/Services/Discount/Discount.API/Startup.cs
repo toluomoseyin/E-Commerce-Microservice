5362d628ea8812e8e254deea2358c36cd61215a4
tolu omoseyin <toluomoseyin01@gmail.com>
2021-08-11 22:18:26 +0100

    made a change

--- src/Services/Discount/Discount.API/Startup.cs
+++ src/Services/Discount/Discount.API/Startup.cs
@@ -38,7 +38,7 @@ namespace Discount.API
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
              if (env.IsDevelopment())
-            {
+            {                                                                  
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Discount.API v1"));
